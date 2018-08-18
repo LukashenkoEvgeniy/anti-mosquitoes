b8043fa3f2bc41825a5e6a7a5d231ff21a033767
Yevhenii <freewindsua@gmail.com>
2018-08-18 14:11:08 +0300

    Add decorator legacy

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env']
-                    }
+                    },
                 }
             },
             {
